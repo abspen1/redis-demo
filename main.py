05a41224a3d72e1796cfb9cecd92dea7d42f93ff
abspen1 <abspencer2097@gmail.com>
2020-12-28 06:54:45 -0700

    update so the restart doesnt happen

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 import redis
+import schedule
+import time
 import os
 
 
-client = redis.Redis(host=os.getenv('REDIS_HOST'), port=6379, db=7,
-                     password=os.getenv("REDIS_PASS"))
+def main():
+    client = redis.Redis(host=os.getenv('REDIS_HOST'), port=6379, db=7,
+                        password=os.getenv("REDIS_PASS"))
 
-client.set('language', 'Python')
+    client.set('language', 'Python')
 
-print(client.get('language'))
+    print(client.get('language'))
+
+
+
+if __name__ == "__main__":
+    main()
+
+schedule.every(5).days.do(main)
+
+while True:
+    try:
+        schedule.run_pending()
+        time.sleep(1)
+    except Exception as identifier:
+        print(identifier)
+        time.sleep(1)

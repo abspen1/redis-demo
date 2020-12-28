d9be77504a1676947d2389f78cd0292682552b29
abspen1 <abspencer2097@gmail.com>
2020-12-28 07:04:46 -0700

    dbsize

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ def main():
     client = redis.Redis(host=os.getenv('REDIS_HOST'), port=6379, db=7,
                         password=os.getenv("REDIS_PASS"))
 
-    client.set('language', 'Python')
-
     print(client.get('language'))
+    print(client.dbsize())
+
+    
 
 
 
